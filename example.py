4b5a3b98d55b02a44fa4d6dc95dac8131b95412a
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-03-27 06:03:53 +0530

    Add comments to example to explain config usage.

--- example.py
+++ example.py
@@ -21,14 +21,21 @@ def run_node():
 
     class ExampleApplication(BaseIOTNodeApplication):
         def build(self):
-            # This is an emergency-only approach. In general, configure roots
-            # in the appropriate node classes instead.
+            # This is an emergency-only approach. In general, configure
+            # roots in the appropriate node classes instead.
             # ( for ex, see BaseIoTNodeGui.install() )
             r = super(ExampleApplication, self).build()
             self._config.register_application_root(
                 os.path.abspath(os.path.dirname(__file__))
             )
             return r
+
+        def on_start(self):
+            # Config is ready by this point, as long as config elements and
+            # application are roots are all registered in the install()
+            # call-chain.
+            self._config.print()
+            super(ExampleApplication, self).on_start()
 
     print("Creating Application : {}".format(ExampleApplication))
     app = ExampleApplication(config=nodeconfig)
