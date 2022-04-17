f515109ad2abb894bde015c8b97950266ccf46fc
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-04-18 01:43:58 +0530

    Minor cleanup of application startup sequencing example.

--- ebs/linuxnode/gui/kivy/utils/application.py
+++ ebs/linuxnode/gui/kivy/utils/application.py
@@ -20,14 +20,21 @@ class BaseIOTNodeApplication(App):
         self._node = self._node_class(reactor=reactor, application=self)
         print("Installing Node Resources")
         self._node.install()
+
+        # Config is ready by this point, as long as config elements and
+        # application roots are all registered in the install()
+        # call-chain.
+        self._config.print()
+
+        print("Using Application Roots :")
+        for root in self._config.roots:
+            print("  ", root)
+
         print("Building GUI for node {0}".format(self._node))
         return self._node.gui_setup()
 
     def on_start(self):
         print("Starting Application : {}".format(self))
-        print("Using Application Roots :")
-        for root in self._config.roots:
-            print("  ", root)
         self._node.start()
 
     def on_stop(self):
